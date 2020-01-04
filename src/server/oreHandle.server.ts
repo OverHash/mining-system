832967109ee6b7189cfd30fcac9b9a1411e38e6f
OverHash <46231745+OverHash@users.noreply.github.com>
2020-01-04 23:36:03 +1300

    Create oreHandle.server.ts

---
+++ src/server/oreHandle.server.ts
@@ -0,0 +1,33 @@
+import { createServerRemoteEvents } from "@rbxts/remoteevent";
+import { remotes, isOre } from "types/global";
+import { Players } from "@rbxts/services";
+import { settings } from "types/settings";
+import { getBlockFromOre } from "./block";
+
+const mainRemote = createServerRemoteEvents(remotes);
+
+mainRemote.server.destroyOre((player, ore) => {
+	if (!isOre(ore)) return;
+
+	const character = player.Character;
+	if (!character) return;
+
+	const humanoid = character.WaitForChild('Humanoid');
+	if (!(humanoid && classIs(humanoid, 'Humanoid'))) return;
+
+	const humanoidRootPart = humanoid.RootPart;
+	if (!humanoidRootPart) return;
+
+	const distance = humanoidRootPart.Position.sub(ore.Position).Magnitude;
+	
+	// check if out of reach
+	if (distance > settings.clickDistance) return;
+
+	// it's in reach, destroy.
+	const block = getBlockFromOre(ore);
+
+	if (block) { 
+		print(block.block.Name + ' exists, destroying it!');
+		block.destroy();
+	}
+})
