812f9516c95f7b898953ee1601f4ae7583f31481
OverHash <46231745+OverHash@users.noreply.github.com>
2020-05-23 14:50:51 +1200

    Update altitude.client.tsx

--- src/client/altitude.client.tsx
+++ src/client/altitude.client.tsx
@@ -1,6 +1,6 @@
-import * as Roact from "@rbxts/roact";
-import { Players, RunService } from "@rbxts/services";
-import { settings } from "types/settings";
+import * as Roact from '@rbxts/roact';
+import { Players, RunService } from '@rbxts/services';
+import { settings } from 'types/settings';
 
 const player = Players.LocalPlayer;
 
@@ -9,30 +9,32 @@ const playerGui = player.WaitForChild('PlayerGui');
 let currentDepth = '0';
 
 interface AltitudeProps {
-	depth: string
+	depth: string;
 }
 
-print(Enum.UserInputState)
+print(Enum.UserInputState);
 
 function Altitude({ depth }: AltitudeProps) {
-	return (<screengui ZIndexBehavior={Enum.ZIndexBehavior.Sibling} Key="AltitudeGui" ResetOnSpawn={false}>
-		<textlabel
-			Font={Enum.Font.SourceSans}
-			Text={`Depth: ${depth}`}
-			TextColor3={Color3.fromRGB(255, 255, 255)}
-			TextScaled
-			TextSize={14}
-			TextWrapped
-			BackgroundTransparency={1}
-			Position={new UDim2(0, 0, 0.925, 0)}
-			Size={new UDim2(0.15, 0, 0.075, 0)}
-			Key="depthLabel"
-		/>
-	</screengui>)
+	return (
+		<screengui ZIndexBehavior={Enum.ZIndexBehavior.Sibling} Key="AltitudeGui" ResetOnSpawn={false}>
+			<textlabel
+				Font={Enum.Font.SourceSans}
+				Text={`Depth: ${depth}`}
+				TextColor3={Color3.fromRGB(255, 255, 255)}
+				TextScaled
+				TextSize={14}
+				TextWrapped
+				BackgroundTransparency={1}
+				Position={new UDim2(0, 0, 0.925, 0)}
+				Size={new UDim2(0.15, 0, 0.075, 0)}
+				Key="depthLabel"
+			/>
+		</screengui>
+	);
 }
 
 function createAltitudeComponent(): Roact.Element {
-	return <Altitude depth={currentDepth}/>
+	return <Altitude depth={currentDepth} />;
 }
 
 let handle = Roact.mount(createAltitudeComponent(), playerGui, 'depthDisplay');
@@ -43,16 +45,15 @@ RunService.RenderStepped.Connect(() => {
 
 	const humanoidRootPart = character.FindFirstChild('HumanoidRootPart');
 	if (!humanoidRootPart || !humanoidRootPart.IsA('BasePart')) return;
-	
+
 	// calculate humanoid height adjusting it based on values
-	const characterDepth = math.clamp((humanoidRootPart.Position.Y - humanoidRootPart.Size.Y / 2, settings.blockSize / 2), -math.huge, 0);
-	const characterBlockDepth = math.abs(characterDepth / settings.blockSize)
+	const humanoidRootCenter = humanoidRootPart.Position.Y - humanoidRootPart.Size.Y / 2;
+
+	const characterDepth = math.clamp(humanoidRootCenter - settings.blockSize / 2, -math.huge, 0);
+	const characterBlockDepth = math.abs(characterDepth / settings.blockSize);
 
 	currentDepth = tostring('%0.0f').format(math.clamp(characterBlockDepth, 0, math.huge));
 
 	// update roact display
-	handle = Roact.update(
-		handle,
-		createAltitudeComponent()
-	)
-})
+	handle = Roact.update(handle, createAltitudeComponent());
+});
